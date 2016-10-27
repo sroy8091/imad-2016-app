723a1875a5c562c70d754c752f49f99c28b4cc26
sroy8091 <sroy8091@gmail.com>
2016-10-27 20:08:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,15 @@
 console.log('Loaded!');
+
+
+var marginLeft = 0;
+var imge = document.getElementById('madi');
+
+function moveRight(marginLeft){
+	marginLeft = marginLeft + 5;
+	console.log(marginLeft);
+	imge.style.marginLeft = marginLeft+'px';
+}
+
+imge.onclick = function(){
+	var interval = setInterval(moveRight, 100);
+};
