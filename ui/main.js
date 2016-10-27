964787dd91b3eb288e646d2871185c202dc81c5c
sroy8091 <sroy8091@gmail.com>
2016-10-28 01:00:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,21 +6,32 @@ var img = document.getElementById('mad');
 var c = document.getElementById('counter');
 var counter = 0;
 
+//making image move right
 function moveRight(){
 	marginLeft = marginLeft + 5;
 
 	img.style.marginLeft = marginLeft + 'px';
 }
 
+//onclicking image after 100ms moveRight function will be called
 img.onclick = function(){
 	var interval = setInterval(moveRight, 100);
 // img.style.marginLeft = '100px';
 };
 
-c.onclick = function(){
-    c.innerHTML = "Clicked";
-    counter = counter + 1;
-    s = document.getElementById('t');
-    // c.value = counter.toString();
-    s.innerHTML = counter.toString();
+//creating request
+
+var request = new XMLHttpRequest();
+
+request.onreadystatechange = function(){
+  if (request.readyState==XMLHttpRequest.DONE){
+      var counter = request.responseText;
+      s = document.getElementById('t');
+      s.innerHTML = counter.toString();
+  }  
 };
+
+//making request
+
+request.open('GET', 'http://sroy8091.cloud.hasura-app.io/counter', true);
+request.send;
