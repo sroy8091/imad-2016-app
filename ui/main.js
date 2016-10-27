d293558c3072569371a6b063aad84874ab5f8e1d
sroy8091 <sroy8091@gmail.com>
2016-10-27 20:26:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,11 +5,11 @@ var marginLeft = 10;
 var img = document.getElementById('mad');
 
 function moveRight(marginLeft){
-	marginLeft = marginLeft + 5;
-	console.log(marginLeft.toString());
-	img.style.marginLeft = marginLeft+'px';
+// 	marginLeft = marginLeft + 5;
+// 	console.log(marginLeft.toString());
+	img.style.marginLeft = '100px';
 }
 
-img.onclick = function(){
-	var interval = setInterval(moveRight, 100);
-};
+// img.onclick = function(){
+// 	var interval = setInterval(moveRight, 100);
+// };
