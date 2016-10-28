3fd37624eb3106d5d24655183534795ec241b14a
sroy8091 <sroy8091@gmail.com>
2016-10-28 08:36:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,9 +17,9 @@ app.get('/counter', function(req, res){
 });
 
 var names = [];
-app.get('/submit-name/:name', function(req, res){ //url=submit-name?name=xxxx
+app.get('/submit-name', function(req, res){ //url=submit-name?name=xxxx
     //for params url app.get('submit-name/:name') and req.params.name
-    var name = req.params.name;
+    var name = req.query.name;
     names.push(name);
     res.send(JSON.stringify(names));
 });
