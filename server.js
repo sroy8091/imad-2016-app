8ab67b0cdc0a68c90c61524ce97c82c6ce0ba650
sroy8091 <sroy8091@gmail.com>
2016-11-23 07:28:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ app.get('/counter', function(req, res){
 
 function hash(input, salt){
   var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
-  return hashed.toString('hex');
+  return [salt, hashed.toString('hex')];
 }
 
 app.get('/hash/:input', function(req, res){
