3b7e82e991d90e322296152ad09e6039a4a048b9
Sumit Roy <sroy8091@gmail.com>
2016-11-23 09:08:21 +0530

    login ui

--- ui/main.js
+++ ui/main.js
@@ -43,22 +43,26 @@ button.onclick = function(){
     var request = new XMLHttpRequest();
     
     request.onreadystatechange = function(){
-      if (request.readyState==XMLHttpRequest.DONE && request.status==200){
-          var names = request.responseText;
-          names = JSON.parse(names);
-          var list = '';
-        for(var i=0; i<names.length; i++){
-              list += '<li>'+names[i]+'</li>';
-        }
-        document.getElementById('list').innerHTML = list;
+      if (request.readyState==XMLHttpRequest.DONE){
+          if(request.status==200){
+            alert('Login successfully');
+          }
+          if(request.status==403){
+            alert('Username or password is incorrect');
+          }
+          else{
+            alert('Server error');
+          }
       }  
     };
     
-    var nameInput = document.getElementById('name');
-    var name = nameInput.value;
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+
     //making request
     
-    request.open('GET', 'http://sroy8091.imad.hasura-app.io/submit-name?name='+name, true);
-    request.send(null);
+    request.open('POST', 'http://sroy8091.imad.hasura-app.io/login', true);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.send(JSON.stringify({username:username, password:password});
 };
 
