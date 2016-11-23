2c73278566c30a5488545abf1a5b60e113eb4208
sroy8091 <sroy8091@gmail.com>
2016-11-23 06:56:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var crypto = require('crypto');
 var bodyParser = require('bodyParser');
 var pool = new Pool({
   user: 'sroy8091',
-  password: 'db-sroy8091-5938',
+  password: process.env.DB_PASSWORD,
   host: 'db.imad.hasura-app.io',
   database: 'sroy8091',
   max: 10, // max number of clients in pool
