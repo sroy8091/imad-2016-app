5616d7a1beaff25ac0de4a3969cdac0333830d4d
sroy8091 <sroy8091@gmail.com>
2016-11-25 20:26:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -28,32 +28,49 @@ app.use(session({
 var counter = 0;
 // var pool = new Pool(config)
 
-function createTemplate(data){
-    var title=data.title;
-    var date=data.date;
-    var heading=data.heading;
-    var content=data.content;
-    var templateOne=
-    `<html>
-         <head>
-             <link href="/ui/style.css" rel="stylesheet" />
-                <title>${title}</title>
-            <meta name="viewport" content="width=device-width, initial-scale=1"/>
-    </head>
-    <body>
-        <div class="container">
-            <div><small>${date}</small></div>
-            <div>
-                <h4>${heading}</h4>
-            </div>
-            <div>
+function createTemplate (data) {
+    var title = data.title;
+    var date = data.date;
+    var heading = data.heading;
+    var content = data.content;
+    
+    var htmlTemplate = `
+    <html>
+      <head>
+          <title>
+              ${title}
+          </title>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link href="/ui/style.css" rel="stylesheet" />
+      </head> 
+      <body>
+          <div class="container">
+              <div>
+                  <a href="/">Home</a>
+              </div>
+              <hr/>
+              <h3>
+                  ${heading}
+              </h3>
+              <div>
+                  ${date.toDateString()}
+              </div>
+              <div>
                 ${content}
-            </div>
-        </div>
-    </body>
-</html>
-`;
-return templateOne;
+              </div>
+              <hr/>
+              <h4>Comments</h4>
+              <div id="comment_form">
+              </div>
+              <div id="comments">
+                <center>Loading comments...</center>
+              </div>
+          </div>
+          <script type="text/javascript" src="/ui/article.js"></script>
+      </body>
+    </html>
+    `;
+    return htmlTemplate;
 }
 
 app.get('/', function (req, res) {
