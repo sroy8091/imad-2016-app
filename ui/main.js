136752e730c012c1ac45ec596f6203596975e06a
sroy8091 <sroy8091@gmail.com>
2016-11-25 20:23:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -42,54 +42,53 @@ var c = document.getElementById('counter');
 window.onload = function(){
 var button = document.getElementById('submit');
 
-button.onclick = function(){
+// button.onclick = function(){
     
-    var request = new XMLHttpRequest();
+//     var request = new XMLHttpRequest();
     
-    request.onreadystatechange = function(){
-      if (request.readyState==XMLHttpRequest.DONE){
-          if(request.status==200){
-            alert('Login successfully');
-          }
-          else if(request.status==403){
-            alert('Username or password is incorrect');
-          }
-          else if(request.status==500){
-            alert('Server error');
-          }
-          else if (request.status==502){
-            alert(502);
-          }
-      }  
-    };
+//     request.onreadystatechange = function(){
+//       if (request.readyState==XMLHttpRequest.DONE){
+//           if(request.status==200){
+//             alert('Login successfully');
+//           }
+//           else if(request.status==403){
+//             alert('Username or password is incorrect');
+//           }
+//           else if(request.status==500){
+//             alert('Server error');
+//           }
+//           else if (request.status==502){
+//             alert(502);
+//           }
+//       }  
+//     };
     
-    var username = document.getElementById('username').value;
-    console.log(username);
-    var password = document.getElementById('password').value;
+//     var username = document.getElementById('username').value;
+//     console.log(username);
+//     var password = document.getElementById('password').value;
 
-    //making request
+//     //making request
     
-    request.open('POST', '/login', true);
-    // request.open('POST', 'localhost:8080/login', true)
-    request.setRequestHeader('Content-Type', 'application/json');
-    request.send(JSON.stringify({username:username, password:password}));
-};
+//     request.open('POST', '/login', true);
+//     // request.open('POST', 'localhost:8080/login', true)
+//     request.setRequestHeader('Content-Type', 'application/json');
+//     request.send(JSON.stringify({username:username, password:password}));
+// };
 
 
-var currentArticleTitle = window.location.pathname.split('/')[2];
-
-function loadCommentForm () {
-    var commentFormHtml = `
-        <h5>Submit a comment</h5>
-        <textarea id="comment_text" rows="5" cols="100" placeholder="Enter your comment here..."></textarea>
-        <br/>
-        <input type="submit" id="submit" value="Submit" />
-        <br/>
+function loadLoginForm () {
+    var loginHtml = `
+        <h3>Login/Register to unlock awesome features</h3>
+        <input type="text" id="username" placeholder="username" />
+        <input type="password" id="password" />
+        <br/><br/>
+        <input type="submit" id="login_btn" value="Login" />
+        <input type="submit" id="register_btn" value="Register" />
         `;
-    document.getElementById('comment_form').innerHTML = commentFormHtml;
+    document.getElementById('login_area').innerHTML = loginHtml;
     
     // Submit username/password to login
-    var submit = document.getElementById('submit');
+    var submit = document.getElementById('login_btn');
     submit.onclick = function () {
         // Create a request object
         var request = new XMLHttpRequest();
@@ -97,26 +96,73 @@ function loadCommentForm () {
         // Capture the response and store it in a variable
         request.onreadystatechange = function () {
           if (request.readyState === XMLHttpRequest.DONE) {
-                // Take some action
-                if (request.status === 200) {
-                    // clear the form & reload all the comments
-                    document.getElementById('comment_text').value = '';
-                    loadComments();    
-                } else {
-                    alert('Error! Could not submit comment');
-                }
-                submit.value = 'Submit';
+              // Take some action
+              if (request.status === 200) {
+                  submit.value = 'Sucess!';
+              } else if (request.status === 403) {
+                  submit.value = 'Invalid credentials. Try again?';
+              } else if (request.status === 500) {
+                  alert('Something went wrong on the server');
+                  submit.value = 'Login';
+              } else {
+                  alert('Something went wrong on the server');
+                  submit.value = 'Login';
+              }
+              loadLogin();
+          }  
+          // Not done yet
+        };
+        
+        // Make the request
+        var username = document.getElementById('username').value;
+        var password = document.getElementById('password').value;
+        console.log(username);
+        console.log(password);
+        request.open('POST', '/login', true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify({username: username, password: password}));  
+        submit.value = 'Logging in...';
+        
+    };
+    
+    var register = document.getElementById('register_btn');
+    register.onclick = function () {
+        // Create a request object
+        var request = new XMLHttpRequest();
+        
+        // Capture the response and store it in a variable
+        request.onreadystatechange = function () {
+          if (request.readyState === XMLHttpRequest.DONE) {
+              // Take some action
+              if (request.status === 200) {
+                  alert('User created successfully');
+                  register.value = 'Registered!';
+              } else {
+                  alert('Could not register the user');
+                  register.value = 'Register';
+              }
           }
         };
         
         // Make the request
-        var comment = document.getElementById('comment_text').value;
-        request.open('POST', '/submit-comment/' + currentArticleTitle, true);
+        var username = document.getElementById('username').value;
+        var password = document.getElementById('password').value;
+        console.log(username);
+        console.log(password);
+        request.open('POST', '/create-user', true);
         request.setRequestHeader('Content-Type', 'application/json');
-        request.send(JSON.stringify({comment: comment}));  
-        submit.value = 'Submitting...';
-        
+        request.send(JSON.stringify({username: username, password: password}));  
+        register.value = 'Registering...';
+    
     };
+}
+
+function loadLoggedInUser (username) {
+    var loginArea = document.getElementById('login_area');
+    loginArea.innerHTML = `
+        <h3> Hi <i>${username}</i></h3>
+        <a href="/logout">Logout</a>
+    `;
 }
 
 function loadLogin () {
@@ -125,49 +171,14 @@ function loadLogin () {
     request.onreadystatechange = function () {
         if (request.readyState === XMLHttpRequest.DONE) {
             if (request.status === 200) {
-                loadCommentForm(this.responseText);
+                loadLoggedInUser(this.responseText);
+            } else {
+                loadLoginForm();
             }
         }
     };
     
     request.open('GET', '/check-login', true);
-    request.send(null);
-}
-
-function escapeHTML (text)
-{
-    var $text = document.createTextNode(text);
-    var $div = document.createElement('div');
-    $div.appendChild($text);
-    return $div.innerHTML;
-}
-
-function loadComments () {
-        // Check if the user is already logged in
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function () {
-        if (request.readyState === XMLHttpRequest.DONE) {
-            var comments = document.getElementById('comments');
-            if (request.status === 200) {
-                var content = '';
-                var commentsData = JSON.parse(this.responseText);
-                for (var i=0; i< commentsData.length; i++) {
-                    var time = new Date(commentsData[i].timestamp);
-                    content += `<div class="comment">
-                        <p>${escapeHTML(commentsData[i].comment)}</p>
-                        <div class="commenter">
-                            ${commentsData[i].username} - ${time.toLocaleTimeString()} on ${time.toLocaleDateString()} 
-                        </div>
-                    </div>`;
-                }
-                comments.innerHTML = content;
-            } else {
-                comments.innerHTML('Oops! Could not load comments!');
-            }
-        }
-    };
-    
-    request.open('GET', '/get-comments/' + currentArticleTitle, true);
     request.send(null);
 }
 
@@ -185,10 +196,10 @@ function loadArticles () {
                     <a href="/articles/${articleData[i].title}">${articleData[i].heading}</a>
                     (${articleData[i].date.split('T')[0]})</li>`;
                 }
-                content += "</ul>";
+                content += "</ul>"
                 articles.innerHTML = content;
             } else {
-                articles.innerHTML('Oops! Could not load all articles!');
+                articles.innerHTML('Oops! Could not load all articles!')
             }
         }
     };
@@ -200,7 +211,9 @@ function loadArticles () {
 
 // The first thing to do is to check if the user is logged in!
 loadLogin();
-loadComments();
+
+// Now this is something that we could have directly done on the server-side using templating too!
+loadArticles();
 
 
 
