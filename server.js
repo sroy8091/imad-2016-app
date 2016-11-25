1b0cafc92552ed7b88ff0096c27847aa251f3761
sroy8091 <sroy8091@gmail.com>
2016-11-25 19:55:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -147,7 +147,7 @@ app.get('/check-login', function (req, res) {
    }
 });
 
-app.get('logout', function(req, res){
+app.get('/logout', function(req, res){
     delete req.session.auth;
     res.send('Logged out successfully');
 })
