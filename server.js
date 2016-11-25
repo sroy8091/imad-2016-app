21369b4751e3a9cbd410cd93a6f76c63c21cae58
sroy8091 <sroy8091@gmail.com>
2016-11-25 19:43:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -112,7 +112,7 @@ app.post('/login', function (req, res) {
           } else {
               // Match the password
               var dbString = result.rows[0].password;
-              var salt = dbString.split('$')[2];
+              var salt = dbString.split('$')[0];
               var hashedPassword = hash(password, salt); // Creating a hash based on the password submitted and the original salt
               if (hashedPassword === dbString) {
                 
