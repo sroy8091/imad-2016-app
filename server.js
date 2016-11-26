5949d4c47995417d02233032ce3e9e2e82b54934
sroy8091 <sroy8091@gmail.com>
2016-11-26 13:32:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -41,6 +41,7 @@ function createTemplate (data) {
               ${title}
           </title>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link href="https://fonts.googleapis.com/css?family=Open+Sans|Roboto" rel="stylesheet">
           <link href="/ui/style.css" rel="stylesheet" />
       </head> 
       <body>
