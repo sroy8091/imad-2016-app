f0b9db631531d6a431bdd6b6ab7ed0380e1da3a9
sroy8091 <sroy8091@gmail.com>
2016-11-26 11:47:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ function createTemplate (data) {
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link href="/ui/style.css" rel="stylesheet" />
       </head> 
-      <body>
+      <body style="background: url("https://s17.postimg.org/5cxaj2ma7/Article.jpg") center center fixed">
           <div class="container">
               <div>
                   <a href="/">Home</a>
