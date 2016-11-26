14b7f4c22929442624d46ac24e324a8a8587cec0
sroy8091 <sroy8091@gmail.com>
2016-11-26 10:24:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -167,24 +167,24 @@ app.get('/check-login', function (req, res) {
 app.get('/logout', function(req, res){
     delete req.session.auth;
     res.send('Logged out successfully');
-})
-
-app.get('/articles/:articleName', function (req, res) {
-    pool.query("SELECT * FROM article where title=$1", [req.params.articleName], function(err, result){
-      if (err){
-        res.status(500).send(err.toString());
-      }
-      else{
-        if (result.rows.length===0){
-          res.status(404).send('Article not found');
-        }
-        else{
-          var articleData = result.rows[0];
-          res.send(createTemplate(articleData));
-        }
-      }
-  });
 });
+
+// app.get('/articles/:articleName', function (req, res) {
+//     pool.query("SELECT * FROM article where title=$1", [req.params.articleName], function(err, result){
+//       if (err){
+//         res.status(500).send(err.toString());
+//       }
+//       else{
+//         if (result.rows.length===0){
+//           res.status(404).send('Article not found');
+//         }
+//         else{
+//           var articleData = result.rows[0];
+//           res.send(createTemplate(articleData));
+//         }
+//       }
+//   });
+// });
 
 var names = [];
 app.get('/submit-name', function(req, res){ //url=submit-name?name=xxxx
